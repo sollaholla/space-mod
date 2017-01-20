c6d60ac41ff22b40d0a7ce7729518b16f8a6a893
Soloman Northrop <sxnorthrop15@gmail.com>
2017-01-20 12:28:24 -0500

    Added new playerprefs class for future upgrades and resource gathering

---
+++ SpaceMod/DataClasses/PlayerPrefs.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceMod.DataClasses
+{
+    public enum VehicleUpgrade
+    {
+        Engine1,
+        Engine2,
+        Engine3,
+        WarpDrive,
+        Lazers
+    }
+
+    public enum Resource
+    {
+        Helium3,
+        Uranium,
+        Diamond,
+        Co2Deposits,
+        NitrogenDeposits,
+        DarkMatter,
+        QuantumParticles,
+        Titanium,
+        WeaponParts
+    }
+
+    [Serializable]
+    public class VehicleUpgradeData
+    {
+        public VehicleUpgradeData(VehicleUpgrade upgrade, bool on)
+        {
+            Upgrade = upgrade;
+            On = on;
+        }
+
+        public VehicleUpgrade Upgrade { get; set; }
+        public bool On { get; set; }
+    }
+
+    [Serializable]
+    public class ResourceData
+    {
+        public ResourceData(Resource resource, int amount)
+        {
+            Resource = resource;
+            Amount = amount;
+        }
+
+        public Resource Resource { get; set; }
+        public int Amount { get; set; }
+    }
+
+    [Serializable]
+    public class PlayerPrefs
+    {
+        public PlayerPrefs()
+        {
+            CreateVehicleUpgrades();
+            CreateResources();
+        }
+
+        public List<VehicleUpgradeData> VehicleUpgrades { get; set; }
+        public List<ResourceData> Resources { get; set; }
+
+        private void CreateResources()
+        {
+            Resources = new List<ResourceData>();
+            var values = (Resource[])Enum.GetValues(typeof(Resource));
+            var toList = values.ToList();
+            toList.ForEach(resource => Resources.Add(new ResourceData(resource, 0)));
+        }
+
+        private void CreateVehicleUpgrades()
+        {
+            VehicleUpgrades = new List<VehicleUpgradeData>();
+            var values = (VehicleUpgrade[])Enum.GetValues(typeof(VehicleUpgrade));
+            var toList = values.ToList();
+            toList.ForEach(upgrade => VehicleUpgrades.Add(new VehicleUpgradeData(upgrade, false)));
+        }
+
+        public bool IsVehicleUpgradeActive(VehicleUpgrade upgrade)
+        {
+            var item = VehicleUpgrades.Find(u => u.Upgrade == upgrade);
+            return item != null && item.On;
+        }
+
+        public void SetUpgradeOn(VehicleUpgrade upgrade)
+        {
+            var item = VehicleUpgrades.Find(u => u.Upgrade == upgrade);
+            if (item == null) return;
+            item.On = true;
+        }
+    }
+}
