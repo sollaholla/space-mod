3d3bd7dc5d1bbe1a2040e64a18ca1f17fae34358
Soloman Northrop <sxnorthrop15@gmail.com>
2017-01-25 09:23:24 -0500

    comments

--- SpaceMod/DataClasses/SceneTypes/MoonOrbitScene.cs
+++ SpaceMod/DataClasses/SceneTypes/MoonOrbitScene.cs
@@ -18,16 +18,19 @@ namespace SpaceMod.DataClasses.SceneTypes
 
         public override void Init()
         {
+            // Create props.
             _earth = World.CreateProp(Constants.EarthMedModel, Vector3.Zero, false, false);
             _moon = World.CreateProp(Constants.MoonLargeModel, Vector3.Zero, false, false);
             var sun = World.CreateProp(Constants.SunSmallModel, Vector3.Zero, false, false);
             var galaxy = World.CreateProp(Constants.SpaceDomeModel, Vector3.Zero, false, false);
             
+            // Move the player to v3.zero so that we can spawn these props.
             ResetPlayerOrigin();
 
+            // Create planets and stars.
             var planets = new List<Planet>
             {
-                new Planet(_earth.Handle, PlayerPed, Vector3.Zero, -3.0f) /*Earth*/,
+                new Planet(_earth.Handle, galaxy, Vector3.Zero, -3.0f) /*Earth*/,
                 new Planet(_moon.Handle, galaxy, Vector3.Zero, 3.5f) /*Moon*/
             };
             var stars = new List<Star>
@@ -35,17 +38,23 @@ namespace SpaceMod.DataClasses.SceneTypes
                 new Star(sun.Handle, Constants.SunOffsetNearEarth) /*Sun*/
             };
 
+            // Move player back to the galaxy center.
             TeleportPlayerToGalaxy();
 
+            // Set the position of the earth and moon.
             _earth.Position = Positions[0];
             _moon.Position = Positions[1];
 
+            // Reset the suns position.
             sun.Position = Constants.GalaxyCenter;
+
+            // Create the planet system.
             _planetSystem = new PlanetSystem(galaxy.Handle, planets, stars, -1.5f);
         }
 
         private void GoToMoon()
         {
+            // Travel to the moon.
             var dist = PlayerPosition.DistanceTo(_moon.Position);
             if (dist > 2000) return;
             End(new MoonSurfaceScene());
@@ -53,13 +62,19 @@ namespace SpaceMod.DataClasses.SceneTypes
 
         public override void Update()
         {
+            // Process the planets and stars.
             _planetSystem.Process(Constants.GetValidGalaxyDomePosition(PlayerPed));
+
+            // Try to go to the moon.
             GoToMoon();
+
+            // Try to leave orbit.
             LeaveOrbit();
         }
 
         private void LeaveOrbit()
         {
+            // Try to leave the moons orbit.
             var dist = PlayerPosition.DistanceTo(_earth.Position);
             if (dist > 2500) return;
             End(new EarthOrbitScene());
@@ -67,12 +82,12 @@ namespace SpaceMod.DataClasses.SceneTypes
 
         public override void Abort()
         {
-            _planetSystem.Abort();
+            _planetSystem?.Abort();
         }
 
         public override void CleanUp()
         {
-            _planetSystem.Abort();
+            _planetSystem?.Abort();
         }
     }
 }
