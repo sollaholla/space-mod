c67f50a4c66702f63ac3410333502765415b15de
ahk1221 <ahk1221@teamcoremods.com>
2017-01-25 22:10:30 +0500

    some random fixes

--- SpaceMod/DataClasses/MissionTypes/TakeBackWhatsOurs.cs
+++ SpaceMod/DataClasses/MissionTypes/TakeBackWhatsOurs.cs
@@ -59,6 +59,9 @@ namespace SpaceMod.DataClasses.MissionTypes
             {
                 var position = origin.Position.Around(_random.Next(75, 80));
                 position = position.MoveToGroundArtificial();
+
+                position = position + new Vector3(0, 0, 15);
+
                 var spaceCraft = World.CreateProp("ufo_zancudo", Vector3.Zero, false, false);
                 spaceCraft.IsPersistent = true;
                 spaceCraft.FreezePosition = true;
