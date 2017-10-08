e0f63ce4eb8a9d8284c1eff4617d43a444e1d5d2
Soloman N <sxnorthrop15@gmail.com>
2017-10-08 13:50:48 -0400

    Add alien class

---
+++ AmbientEnemySpawns/Alien.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using GTA;
+using GTA.Math;
+using GTA.Native;
+
+namespace AmbientEnemySpawns
+{
+    public class Alien : Entity
+    {
+        private readonly Ped _parent;
+        private readonly float _combatRange;
+
+        private bool _initializedAi;
+        private bool _isGoingToEnemy;
+        private DateTime _goToTimeout;
+        private bool _isAttackingEnemy;
+
+        public Alien(int handle, float combatRange) : base(handle)
+        {
+            _parent = new Ped(handle);
+            _combatRange = combatRange * combatRange;
+        }
+
+        public Ped Enemy { get; set; }
+
+        public void Update()
+        {
+            if (!Exists() || !Exists(_parent)) return;
+            if (IsDead) return;
+
+            if (!_initializedAi)
+            {
+                InitializeAi();
+                _initializedAi = true;
+            }
+
+            if (!Exists(Enemy))
+            {
+                return;
+            }
+
+            var distanceToEnemy = Position.DistanceToSquared(Enemy.Position);
+
+            if (distanceToEnemy > _combatRange)
+            {
+                _isAttackingEnemy = false;
+                if (!_isGoingToEnemy)
+                {
+                    _parent.Task.RunTo(Enemy.Position, true, -1);
+                    _goToTimeout = DateTime.Now + new TimeSpan(0, 0, 0, 0, 250);
+                    _isGoingToEnemy = true;
+                }
+                else if (DateTime.Now > _goToTimeout)
+                {
+                    _isGoingToEnemy = false;
+                }
+            }
+            else
+            {
+                _isGoingToEnemy = false;
+                if (!_isAttackingEnemy)
+                {
+                    _parent.Task.ClearAll();
+                    _parent.Task.ShootAt(Enemy);
+                    _isAttackingEnemy = true;
+                }
+            }
+        }
+
+        private void InitializeAi()
+        {
+            _parent.BlockPermanentEvents = true;
+            _parent.AlwaysKeepTask = true;
+        }
+
+        public static explicit operator Ped(Alien other)
+        {
+            return new Ped(other.Handle);
+        }
+    }
+}
